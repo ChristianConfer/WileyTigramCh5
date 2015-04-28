e198ecd6ce4d70d7be5bdb4b1c7f83b2d47aca35
ChristianConfer <confer.5.2013@gmail.com>
2015-04-28 17:28:07 -0500

    chpater 6 changes

--- app/controllers/feedRow.js
+++ app/controllers/feedRow.js
@@ -2,6 +2,8 @@ var args = arguments[0] || {};
 
 $.image.image = model.attributes.urls.preview;
 
+//this is a JavaScript feature that says:
+//if the first thing is empty, then use the second thing
 $.titleLabel.text = args.title || "";
 
 $.row_id = model.id || "";
